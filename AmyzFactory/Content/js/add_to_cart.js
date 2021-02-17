f79fff0a99c619c80af843d71e31954222af6d02
amr-ghonimy <amrmhgh1997@gmail.com>
2021-02-17 10:17:42 +0200

    sdad

---
+++ AmyzFactory/Content/js/add_to_cart.js
@@ -0,0 +1,34 @@
+﻿
+$('.add-to-card').click(function(){
+
+    var itemId = $(this).attr("data-catid");
+
+
+    var formData = new FormData();
+    var URL = '/Product/AddToCart'
+    formData.append("itemid", itemId);
+
+    $.ajax({
+        async: true,
+        type: 'Post',
+        contentType: false,
+        processData: false,
+        data: formData,
+        url: URL,
+        success: function (data) {
+            if (data.Success) {
+                $("#counterText").text(data.Counter);
+            }
+        },
+        error: function () {
+            alert("يوجد بعد المشاكل");
+        }
+
+
+
+
+    });
+
+
+    
+})
